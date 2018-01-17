c078da0906291e9f33d6fae9d46d25f566b7fc14
Joe K <joek1301@gmail.com>
2018-01-17 15:13:25 -0500

    Add main entrypoint and set up logging

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,13 @@
-import discord, config
+import discord, config, logging
+from discord.ext import commands
 
 CONFIG = config.load_config() # load the config file
+
+bot = commands.Bot(command_prefix="^")
+
+@bot.event
+async def on_ready():
+    logging.info(f"Logged in as {bot.user.name}")
+
+def run():
+    bot.run(CONFIG["token"])
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import bot, logging
+
+if __name__ == "__main__":
+    formatter = logging.Formatter(fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler("bot.log")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    bot.run()
