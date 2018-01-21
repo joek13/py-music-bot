a45a982ce4b1a3aefec27395cb22c8b1ab63d295
Joe K <joek1301@gmail.com>
2018-01-20 23:04:09 -0500

    Add uptime command

--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -1,10 +1,13 @@
 from discord.ext import commands
 import discord
+from datetime import datetime
+import util
 
 class Meta:
     """Commands relating to the bot itself."""
     def __init__(self, bot):
         self.bot = bot
+        self.start_time = datetime.now()
         bot.remove_command("help")
 
     @commands.command(name="help")
@@ -19,3 +22,9 @@ class Meta:
                 await ctx.send(f"Command or category {command} not found.")
             else:
                 await ctx.send("\n".join(await formatter.format_help_for(ctx, thing)))
+
+    @commands.command()
+    async def uptime(self, ctx):
+        """Tells how long the bot has been running."""
+        uptime_seconds = round((datetime.now() - self.start_time).total_seconds())
+        await ctx.send(f"Current Uptime: {util.format_seconds(uptime_seconds)}")
---
+++ src/util.py
@@ -0,0 +1,20 @@
+# Some generic utility commands.
+
+def format_seconds(time_seconds):
+    """Formats some number of seconds into a string of format d days, x hours, y minutes, z seconds"""
+    seconds = time_seconds
+    hours = 0
+    minutes = 0
+    days = 0
+    while seconds >= 60:
+        if seconds >= 60*60*24:
+            seconds -= 60*60*24
+            days += 1
+        elif seconds >= 60*60:
+            seconds -= 60*60
+            hours += 1
+        elif seconds >= 60:
+            seconds -= 60
+            minutes += 1
+    
+    return f"{days}d {hours}h {minutes}m {seconds}s"
