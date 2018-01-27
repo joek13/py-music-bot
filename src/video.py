82f7dbad6f94bef004774d3d6cb2953cf22a0ec6
Joe K <joek1301@gmail.com>
2018-01-26 20:54:04 -0500

    Playlists no longer break bot

--- src/video.py
+++ src/video.py
@@ -4,7 +4,8 @@ import discord
 YTDL_OPTS = {
         "default_search": "ytsearch",
         "format": "bestaudio/best",
-        "quiet": True
+        "quiet": True,
+        "extract_flat": "in_playlist"
 }
 
 class Video:
@@ -12,12 +13,7 @@ class Video:
     def __init__(self, url_or_search, requested_by):
         """Plays audio from (or searches for) a URL."""
         with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
-            info = ydl.extract_info(url_or_search, download=False)
-            video = None
-            if "_type" in info and info["_type"] == "playlist":
-                video = info["entries"][0]
-            else:
-                video = info
+            video = self._get_info(url_or_search)
             video_format = video["formats"][0]
             self.stream_url = video_format["url"]
             self.video_url = video["webpage_url"]
@@ -25,6 +21,15 @@ class Video:
             self.uploader = video["uploader"] if "uploader" in video else ""
             self.thumbnail = video["thumbnail"] if "thumbnail" in video else None
             self.requested_by = requested_by
+    def _get_info(self, video_url):
+        with ytdl.YoutubeDL(YTDL_OPTS) as ydl:
+            info = ydl.extract_info(video_url, download=False)
+            video = None
+            if "_type" in info and info["_type"] == "playlist":
+                return self._get_info(info["entries"][0]["url"]) # get info for first video
+            else:
+                video = info
+            return video
     def get_embed(self):
         """Makes an embed out of this Video's information."""
         embed = discord.Embed(
