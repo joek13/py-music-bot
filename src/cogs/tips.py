6ac3d575baa224d49e00c233daeff26de08b4b3f
Joe K <joek1301@gmail.com>
2018-12-09 22:20:56 -0500

    Added source code tip

--- src/cogs/tips.py
+++ src/cogs/tips.py
@@ -2,17 +2,16 @@ from discord.ext import commands
 import discord
 import random
 
-TIPS = ["Only admins and the song requester can immediately skip songs. Everybody else will have to vote!", ""]
-
 class Tips:
     """Commands for providing tips about using the bot."""
 
     def __init__(self, bot, config):
         self.bot = bot
         self.config = config[__name__.split(".")[-1]]
+        self.tips = ["Only admins and the song requester can immediately skip songs. Everybody else will have to vote!", f"You can check out my source code here: {self.config['github_url']}"]
 
     @commands.command()
     async def tip(self, ctx):
         """Get a random tip about using the bot."""
-        index = random.randrange(len(TIPS))
-        await ctx.send(f"**Tip #{index+1}:** {TIPS[index]}")
+        index = random.randrange(len(self.tips))
+        await ctx.send(f"**Tip #{index+1}:** {self.tips[index]}")
