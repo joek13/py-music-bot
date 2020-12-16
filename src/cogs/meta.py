823c6826ae365df791985bb945cf5e684c128761
Joe Kerrigan <joek1301@gmail.com>
2020-12-16 18:48:48 -0500

    Remove custom help command so default one can activate

--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -11,22 +11,6 @@ class Meta(commands.Cog):
         self.bot = bot
         self.start_time = datetime.now()
         self.config = config
-        bot.remove_command("help")
-
-    @commands.command(name="help")
-    async def _help(self, ctx, command: str = None):
-        """Gets help for a single command, or an entire category."""
-        formatter = commands.HelpFormatter(show_check_failure=True)
-        if command is None:
-            await ctx.send("\n".join(await formatter.format_help_for(
-                ctx, self.bot)))
-        else:
-            thing = self.bot.get_cog(command) or self.bot.get_command(command)
-            if thing is None:
-                await ctx.send(f"Command or category {command} not found.")
-            else:
-                await ctx.send("\n".join(await formatter.format_help_for(
-                    ctx, thing)))
 
     @commands.command()
     async def uptime(self, ctx):
